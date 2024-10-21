d164f31ff22c739fc45191b34284ca7787b4d173
vinitnprabhu <vinitnprabhu@gmail.com>
2024-10-21 18:57:56 -0400

    clerk authentication setup

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
+
+export const metadata: Metadata = {
+  title: "Genaura",
+  description: "AI Image Generating platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
